171b265797c909baef8c826ebaff7acadbb3c65b
Virendra <virendrayadav.sd@gmail.com>
2024-09-11 02:10:00 +0530

    Final changes

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById('queryType').addEventListener('change', function() {
     let projectOptions = document.getElementById('projectOptions');
     if (this.value === 'project') {
@@ -10,7 +24,66 @@ document.getElementById('queryType').addEventListener('change', function() {
 
 const button = document.getElementById('scroll-top');
 
-button.addEventListener('click', function() {
-    // Scroll to the top of the page
+button.addEventListener('click', () => {
+    console.log('btn-clicked');
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+
+
+
+window.addEventListener('scroll', () => {
+    const headerScroll = document.getElementById('header-id');
+    if (window.scrollY > 100) {
+        headerScroll.style.backgroundColor = 'white';
+    } else {
+        headerScroll.style.backgroundColor = 'transparent';
+    }
+});
+
+
+
+let hamBtn = document.getElementById('ham-button');
+let closeBtn = document.getElementById('close-button');
+let resNav = document.getElementById('resp-nav-bar');
+let textBN = document.getElementById('text-below-nav');
+
+hamBtn.addEventListener('click', () => {
+    console.log('ham btn clicked');
+    resNav.style.display = "block";
+    closeBtn.style.display = "block";
+    hamBtn.style.display = "none";
+    textBN.style.display = "none";
+    
+    
+});
+
+closeBtn.addEventListener('click', () => {
+    console.log('close btn clicked');
+    resNav.style.display = "none";
+    closeBtn.style.display = "none";
+    hamBtn.style.display = "block";
+    textBN.style.display = "block";
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
